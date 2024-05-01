d97eba0706be841be0288a8e1a7541d92889eba0
tetarwalsachinkumar <sachinkumartetarwal1913@gmail.com>
2024-05-01 14:05:04 +0530

    calander added

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ content[1].addEventListener("click",()=>{
     if(k%2==1) message.classList.remove("hide");
     if(k%2==0) message.classList.add("hide");
 });
+
+
+// adding date divs
+const dates=document.querySelector(".dates");
+for(let i=1;i<=35;i++){
+    let datebox=document.createElement("div");
+    datebox.className="date";
+    if(i<=3) {datebox.innerText=""; datebox.style.backgroundColor="#ffcdb2";}
+    else if (i>3 && i<=34) datebox.innerText=i-3;
+    else{ datebox.innerText=""; datebox.style.backgroundColor="#ffcdb2";}
+    dates.appendChild(datebox);
+}
